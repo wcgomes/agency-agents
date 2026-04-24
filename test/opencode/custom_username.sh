f7b4d41114d3fbe17020e15fc8aad274cf8efd62
Wyller Gomes <wyller.gomes@gmail.com>
2026-04-24 17:14:50 +0000

    test: remove marker file check from custom_username

--- test/opencode/custom_username.sh
+++ test/opencode/custom_username.sh
@@ -7,6 +7,4 @@ check "opencode binary exists" test -f /usr/local/bin/opencode
 
 check "opencode-fix-permissions script exists" test -f /usr/local/bin/opencode-fix-permissions
 
-check "marker file exists for vscode user" test -f /usr/local/share/devcontainer-features/opencode-v1-vscode-latest.done
-
 reportResults
